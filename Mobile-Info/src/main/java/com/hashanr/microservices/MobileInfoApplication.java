480009be605310c289f367698f2778c234f71e6f
HashanR <rmhashan@gmail.com>
2020-05-25 03:41:56 +0530

    Fix gradle dependency related bug

--- Mobile-Info/src/main/java/com/hashanr/microservices/MobileInfoApplication.java
+++ Mobile-Info/src/main/java/com/hashanr/microservices/MobileInfoApplication.java
@@ -3,6 +3,7 @@ package com.hashanr.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EntityScan("com.hashanr.microservice.commons.model")
