e180623dbcc04cc40c7de5f07e0e0d20bed40524
HashanR <rmhashan@gmail.com>
2020-05-25 04:49:25 +0530

    Add Global method level permision to enchance security of API

--- Mobile-Info/src/main/java/com/hashanr/microservices/MobileInfoApplication.java
+++ Mobile-Info/src/main/java/com/hashanr/microservices/MobileInfoApplication.java
@@ -3,11 +3,13 @@ package com.hashanr.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EntityScan("com.hashanr.microservice.commons.model")
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MobileInfoApplication {
 
 	public static void main(String[] args) {
--- Mobile-Info/src/main/java/com/hashanr/microservices/controller/SubscriberController.java
+++ Mobile-Info/src/main/java/com/hashanr/microservices/controller/SubscriberController.java
@@ -1,6 +1,7 @@
 package com.hashanr.microservices.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class SubscriberController {
 	SubscriberService subscriberService;
 	
 	@GetMapping("v1/subscriber/{MSISDN}")
+	@PreAuthorize("hasAuthority('create_profile')")
 	public SubscriberData getSubscriber(@PathVariable int MSISDN) {
 		return subscriberService.getSubscriberInfo(MSISDN);
 	}
