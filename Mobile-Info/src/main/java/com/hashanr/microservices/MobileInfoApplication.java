c98a57d195a978434dbf41fd90042f4b94b2d6b3
HashanR <rmhashan@gmail.com>
2020-05-26 08:50:19 +0530

    Downgrade spring boot version so that add swagger, swagger added and allow swagger endpoint from spring security

--- Mobile-Info/src/main/java/com/hashanr/microservices/MobileInfoApplication.java
+++ Mobile-Info/src/main/java/com/hashanr/microservices/MobileInfoApplication.java
@@ -6,11 +6,15 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @SpringBootApplication
 @EntityScan("com.hashanr.microservice.commons.model")
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableSwagger2
 public class MobileInfoApplication {
 
 	public static void main(String[] args) {
